a48656879286eea80f7f90939b0956fb59997cea
Nikita Istomin <nikita.istomin54@gmail.com>
2024-03-15 00:36:17 -0500

    refactor: Test ppr by temporary taking out page revalidation on server actions.

--- src/server/actions.ts
+++ src/server/actions.ts
@@ -116,7 +116,7 @@ export async function login(redirect_uri?: string) {
     maxAge: 60 * 10,
     sameSite: "lax",
   })
-  revalidatePath("/", "layout")
+  // revalidatePath("/", "layout")
   return redirect(url.toString())
 }
 
@@ -136,7 +136,7 @@ export async function logout(): Promise<LogoutActionResult> {
     sessionCookie.value,
     sessionCookie.attributes,
   )
-  revalidatePath("/", "layout")
+  // revalidatePath("/", "layout")
   return {
     error: null,
   }
